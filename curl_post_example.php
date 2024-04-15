a73c2ee78688dd3e648388b886c49b41395032b3
Nguyen Louis <nguyenlouis172@gmail.com>
2024-04-15 09:52:26 +0200

    post data curl

---
+++ curl_post_example.php
@@ -0,0 +1,30 @@
+<?php
+
+$url = '{POST_REST_ENDPOINT}';
+
+// initialise une session cURL
+$curl = curl_init();
+
+// définit l'URL de la session cURL
+$fields = array(
+
+    'field_name_1' => 'Value 1',
+
+    'field_name_2' => 'Value 2',
+
+    'field_name_3' => 'Value 3'
+
+);
+
+$fields_string = http_build_query($fields);
+ 
+curl_setopt($curl, CURLOPT_URL, $url);
+
+curl_setopt($curl, CURLOPT_POST, TRUE);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $fields_string);
+
+$data = curl_exec($curl);
+
+// ferme la session cURL
+curl_close($curl);
