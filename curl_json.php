c9b95af276b39d84f96decdcf1951f85807d6d04
Nguyen Louis <nguyenlouis172@gmail.com>
2024-04-15 10:11:46 +0200

    json data curl

---
+++ curl_json.php
@@ -0,0 +1,35 @@
+<?php
+
+// URL de l'API REST
+$url = '{POST_REST_ENDPOINT}';
+
+
+
+// initialise une session cURL
+$curl = curl_init();
+
+// champs à envoyer
+$fields  = array(
+    'field_name_1' => 'Value 1',
+    'field_name_2' => 'Value 2',
+    'field_name_3' => 'Value 3'
+);
+
+$json_string = json_encode($fields);
+
+// options de la session cURL
+curl_setopt($curl, CURLOPT_URL, 'http://www.google.com');
+curl_setopt($curl, CURLOPT_POST, TRUE);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $json_string);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content: application/json'));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$data = curl_exec($curl);
+
+curl_close($curl);
+
+// exécute la session cURL
+curl_exec($curl);
+
+// ferme la session cURL
+curl_close($curl);
