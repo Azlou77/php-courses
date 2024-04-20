0b7a78541303b5ffc9c03259b097347577b59df7
Nguyen Louis <nguyenlouis172@gmail.com>
2024-04-20 16:31:50 +0200

    add list series categories

--- project/partials/navigation.php
+++ project/partials/navigation.php
@@ -1,32 +1,33 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light bg-succes">
-        <a class="navbar-brand" href="index.php">Accueil</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+    <a class="navbar-brand" href="index.php">Accueil</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
 
-            <!-- Movies -->
-            <div class="dropdown">
-                <button type="button" class="btn btn-white dropdown-toggle" data-bs-toggle="dropdown">
-                    Movies
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Movies favoris</a></li>
-                    <li><a class="dropdown-item" href="#">Movies historic</a></li>
-                    <li><a class="dropdown-item" href="#">Movie fantastic</a></li>
-                </ul>
-            </div>
+        <!-- Movies -->
+        <div class="dropdown">
+            <button type="button" class="btn btn-white dropdown-toggle" data-bs-toggle="dropdown">
+                Movies
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/project/movies_historic.php">Movies historic</a></li>
+                <li><a class="dropdown-item" href="/project/movies_fantastic.php">Movies fantastic</a></li>
+                <li><a class="dropdown-item" href="#">Movies actors</a></li>
+            </ul>
+        </div>
 
-            <!-- Series -->
-            <div class="dropdown">
-                <button type="button" class="btn btn-white dropdown-toggle" data-bs-toggle="dropdown">
-                    Series
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Series favoris</a></li>
-                    <li><a class="dropdown-item" href="#">Series Mangas</a></li>
-                </ul>
-            </div>
-    </nav>
+        <!-- Series -->
+        <div class="dropdown">
+            <button type="button" class="btn btn-white dropdown-toggle" data-bs-toggle="dropdown">
+                Series
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/project/series_fantastic.php">Series fantastic</a></li>
+                <li><a class="dropdown-item" href="#">Series Mangas</a></li>
+                <li><a class="dropdown-item" href="#">Series actors</a></li>
+            </ul>
+        </div>
+</nav>
 <!-- End Navigation -->
--- project/series_fantastic.php
+++ project/series_fantastic.php
@@ -1,34 +1,34 @@
 <?php include 'partials/header.php'; ?>
 
-<body>
-    <?php include 'partials/navigation.php'; ?>
-    <div class="container center">
-        <h2 class="d-flex justify-content-center"> My movies-series list to watch </h2>
+    <body>
+        <?php include 'partials/navigation.php'; ?>
+        <div class="container center">
+        <h2 class="d-flex justify-content-center"> My fantastic series </h2>
         <div class="row">
             <div class="col">
                 <br>
                 <?php
-                //initialise curl
+                // initialise curl
                 $curl = curl_init();
 
-                //Get the list series-movies to watch
-                curl_setopt($curl, CURLOPT_URL, 'https://api.themoviedb.org/3/account/{id_account}/favorite/movies?language=en-US&page=1&sort_by=created_at.asc');
+                 // Get the list series fantastic
+                 curl_setopt($curl, CURLOPT_URL, 'https://api.themoviedb.org/3/list/{id_list-name_list}?api_key={api_key}');
 
                 // Avoid to display errors
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-                // Send request
+                // Send the request
                 $authorization = "Authorization: Bearer {token}";
                 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application /json', $authorization));
 
-                // Execute request
+                //Execute the curl session
                 $resultat = curl_exec($curl);
 
-                // Close request
+                // Close the curl session
                 curl_close($curl);
 
                 $resultat = json_decode($resultat, true);
-                $list = $resultat['results'];
+                $list = $resultat['items'];
 
                 // Browse the list array
                 foreach ($list as $key => $value) {
@@ -37,13 +37,12 @@
                     echo '<div class="card" style="width: 18rem;">
                     <img src="https://image.tmdb.org/t/p/w500' . $value['poster_path'] . '" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">' . $value['original_title'] . '</h5> 
+                        <h5 class="card-title">' . $value['original_name'] . '</h5>
                         <p class="card-text">' . $value['overview'] . '</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>';
                 }
                 ?>
-</body>
-
+    </body>
 </html>
