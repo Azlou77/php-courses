811bfadac9416ac751871c98ad5dd61061fa1b99
Nguyen Louis <nguyenlouis172@gmail.com>
2024-06-16 16:13:00 +0200

    add prepare requests

--- project/POO_MVC/public/connect.php
+++ project/POO_MVC/public/connect.php
@@ -1,5 +1,5 @@
 <?php
-// database variables
+// Database variables
 $dbname="POO_MVC";
 $servername="localhost";
 $username="root";
@@ -13,4 +13,18 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 
+// We retrieve only the last events
+// Prepare the request
+$stmt = $conn->prepare("SELECT title, content  FROM exhibition");
+
+// Execute the request
+$stmt->execute();
+
+/* Loop the list until  the statement(SQL request) find the pair key/value in an array */
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    // Display the key of row, example $student["name"] = "Louis" or "Steven" etc ...
+    echo $row['title'] . "<br>";
+    echo $row['content'] . "<br>";
+
+}
 ?>
